3d88533f8fa8802807c552db95e68d7b99e0f85f
muideen7 <olayeyeayomide2000@gmail.com>
2023-07-10 05:54:59 +0100

    lockboxes-solved

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,15 +5,18 @@ Each box is numbered sequentially from 0 to n - 1
  and each box may contain keys to the other boxes.
 """
 
-
 def canUnlockAll(boxes):
     """
-    boxes is a list of lists
-    :param boxes:
-    :return: boolean
+    Check if all the boxes can be unlocked.
+
+    Args:
+        boxes (list): A list of lists representing the locked boxes.
+
+    Returns:
+        bool: True if all the boxes can be unlocked, False otherwise.
     """
     totalBoxes = len(boxes)
-    checkBox = [False] * totalBoxes
+    checkBox = [False] * total_boxes
     checkBox[0] = True
     stack = [0]
 
@@ -22,7 +25,8 @@ def canUnlockAll(boxes):
         keys = boxes[currentBox]
 
         for key in keys:
-            if key < totalBoxes and not checkBox[key]:
+            if key < total_boxes and not check_box[key]:
                 checkBox[key] = True
                 stack.append(key)
+
     return all(checkBox)
