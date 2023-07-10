4c3bd9377d51c3c1c744a6b2f50dd8f3af94aeb4
muideen7 <olayeyeayomide2000@gmail.com>
2023-07-10 05:44:57 +0100

    lockboxes-solved

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,30 +5,18 @@ Each box is numbered sequentially from 0 to n - 1
  and each box may contain keys to the other boxes.
 """
 def canUnlockAll(boxes):
-    # Set to keep track of opened boxes
-    opened_boxes = set([0])
+    totalBoxes = len(boxes)
+    checkBox = [False] * totalBoxes
+    checkBox[0] = True
+    stack = [0]
 
-    # List to store the keys obtained
-    keys = boxes[0]
+    while stack:
+        currentBox = stack.pop()
+        keys = boxes[currentBox]
 
-    # Iterate over the keys until no new keys are found
-    while keys:
-        new_keys = []
-
-        # Check each key obtained
         for key in keys:
-            # If the key corresponds to a box that hasn't been opened yet
-            if key < len(boxes) and key not in opened_boxes:
-                # Mark the box as opened and add its keys to the list of new keys
-                opened_boxes.add(key)
-                new_keys.extend(boxes[key])
+            if key < totalBoxes and not checkBox[key]:
+                checkBox[key] = True
+                stack.append(key)
 
-        # Update the keys list with the new keys
-        keys = new_keys
-
-    # If all boxes have been opened, return True
-    if len(opened_boxes) == len(boxes):
-        return True
-
-    # If there are still unopened boxes, return False
-    return False
+    return all(checkBox)
