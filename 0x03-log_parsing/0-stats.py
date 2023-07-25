729cde62043a3bdf28c5d7382310b18d51590858
Olayeye Muideen Ayomide <olayeyeayomide2000@gmail.com>
2023-07-25 06:55:49 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import sys
 
-# Initialize variables to store total file size and line count
-total_size = 0
-count = 0
+def compute_metrics():
+    total_size = 0
+    status_codes = {}
 
-# Dictionary to store counts of different status codes
-# The keys are the status codes, and the values are the counts initialized to 0
-codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    try:
+        line_count = 0
+        for line in sys.stdin:
+            line_count += 1
 
-# Function to print the total file size and counts of each status code
-def print_stats():
-    print("File size: {}".format(total_size))
-    for key, val in codes.items():
-        if val:
-            print("{}: {}".format(key, val))
+            # Parse the line using regex
+            match = re.match(r'^(\S+) - \[.*\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$', line.strip())
+            if not match:
+                continue
 
-try:
-    # Read lines from the standard input (stdin)
-    for line in sys.stdin:
-        # Split the line into values based on spaces
-        values = line.split(" ")
+            ip_address, status_code, file_size = match.groups()
 
-        # Check if the line has more than 2 elements (assuming input format)
-        if len(values) > 2:
-            # Extract the status code and file size from the line
-            code = values[-2]
-            size = int(values[-1])
+            # Update total file size
+            total_size += int(file_size)
 
-            # Check if the status code is one of the specified ones
-            if code in codes:
-                # Increment the count for the status code and add the size to total_size
-                codes[code] += 1
-                total_size += size
+            # Update status code count
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            else:
+                status_codes[status_code] = 1
 
-            # Increment the line count
-            count += 1
+            # Print metrics after every 10 lines
+            if line_count % 10 == 0:
+                print_metrics(total_size, status_codes)
 
-            # Check if 10 lines have been processed
-            if count == 10:
-                # Print the current statistics
-                print_stats()
+    except KeyboardInterrupt:
+        # Print final metrics if keyboard interruption occurs
+        print_metrics(total_size, status_codes)
 
-                # Reset the line count for the next 10 lines
-                count = 0
+def print_metrics(total_size, status_codes):
+    print(f"Total file size: File size: {total_size}")
+    for status_code in sorted(status_codes.keys()):
+        print(f"{status_code}: {status_codes[status_code]}")
 
-# Handle keyboard interruption (CTRL + C)
-except KeyboardInterrupt:
-    # Print the current statistics before exiting
-    print_stats()
-
-finally:
-    # Print the final statistics after all lines have been processed
-    print_stats()
+if __name__ == "__main__":
+    compute_metrics()
