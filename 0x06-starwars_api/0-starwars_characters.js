1e9b7bb75ff231afa046898569fd9eeac73d70e9
muideen7 <olayeyeayomide2000@gmail.com>
2023-08-11 06:12:28 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,65 +2,79 @@
 
 const request = require('request');
 
-// Function to fetch movie data
-const fetchMovieData = movieId => {
-  const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let characters = [];
+const characterNames = [];
 
-  return new Promise((resolve, reject) => {
-    request(apiUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`API Error: ${response.statusCode}`));
-      } else {
-        const movieData = JSON.parse(body);
-        resolve(movieData);
-      }
-    });
-  });
-};
-
-// Function to fetch character data for a movie
-const fetchCharacterData = characterUrl => {
-  return new Promise((resolve, reject) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`Character Error: ${response.statusCode}`));
-      } else {
-        const characterData = JSON.parse(body);
-        resolve(characterData);
-      }
-    });
-  });
-};
-
-// Main function to print characters' names
-const printMovieCharacters = async movieId => {
+// Function to request and store character data
+const requestAndStoreCharacters = async () => {
   try {
-    const movieData = await fetchMovieData(movieId);
-    const characters = movieData.characters;
+    // Fetch movie data
+    const movieResponse = await fetchApiData(filmEndPoint);
 
-    console.log(`Characters in "${movieData.title}":`);
-
-    for (let index = 0; index < characters.length; index++) {
-      const characterData = await fetchCharacterData(characters[index]);
-      console.log(`${index + 1}. ${characterData.name}`);
-    }
-
-    console.log('All characters printed.');
+    // Store characters' URLs
+    characters = movieResponse.characters;
   } catch (error) {
-    console.error('Error:', error);
+    handleError(error, 'Fetching movie data');
   }
 };
 
-// Get the movie ID from command line argument
-const movieId = process.argv[2];
+// Function to fetch data from the API
+const fetchApiData = url => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        reject(error || new Error(`API Error: ${response.statusCode}`));
+      } else {
+        const jsonData = JSON.parse(body);
+        resolve(jsonData);
+      }
+    });
+  });
+};
 
-if (!movieId) {
-  console.error('Please provide a movie ID as a command line argument.');
-} else {
-  printMovieCharacters(movieId);
-}
+// Function to request and store character names
+const requestAndStoreNames = async () => {
+  if (characters.length > 0) {
+    for (const characterUrl of characters) {
+      try {
+        // Fetch character data
+        const characterResponse = await fetchApiData(characterUrl);
 
+        // Store character name
+        characterNames.push(characterResponse.name);
+      } catch (error) {
+        handleError(error, 'Fetching character data');
+      }
+    }
+  } else {
+    console.error('Error: Got no characters for some reason');
+  }
+};
+
+// Function to handle errors
+const handleError = (error, context) => {
+  console.error(`Error (${context}):`, error.message);
+};
+
+// Function to display character names
+const displayCharacterNames = () => {
+  for (let i = 0; i < characterNames.length; i++) {
+    if (i === characterNames.length - 1) {
+      process.stdout.write(characterNames[i]);
+    } else {
+      process.stdout.write(characterNames[i] + '\n');
+    }
+  }
+};
+
+// Main function
+const main = async () => {
+  await requestAndStoreCharacters();
+  await requestAndStoreNames();
+
+  displayCharacterNames();
+};
+
+main();
